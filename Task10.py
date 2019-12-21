15eeea2f0a5a67589e3fac53bf1943214df3cd68
Marat-R <you@example.com>
2019-12-21 12:58:08 +0600

    first commit

---
+++ Task10.py
@@ -0,0 +1,20 @@
+word = "Kyrgyzstan"
+# len_ = len(word)
+print("1) Third character - ", word[2])
+print("2) Second to last character - ", word[1:])
+print("3) First five characters - ", word[0:5])
+print("4) All but the last two characters - ", word[0:8])
+print("5) With even indices - ", word[1::2])
+print("6) With odd indices - ", word[::2])
+print("7) Reverse order - ", word[::-1])
+print("8) Every second character in reverse order - ", word[::-2])
+print("9) Length of the given string - ", len(word))
+
+
+# word_list = list(word)
+# word_len = len(word_list)
+# word_index = word_list[3:word_len]
+# list_str = ' '.join(map(str, word_index)) 
+
+# print("1-st line: ", str(word_index))
+# print("2-nd line: ", list_str)
